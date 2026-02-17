95235d399c342217e6ffd82c62541aa3e1400b0e
GrupoTEKC <adhesivospegatek@gmail.com>
2026-02-17 14:33:38 -0600

    Update ModalEntrega.jsx

--- erp-frontend/src/pages/ModalEntrega.jsx
+++ erp-frontend/src/pages/ModalEntrega.jsx
@@ -13,7 +13,7 @@ useEffect(() => {
       productos.map(p => ({
         id_producto: p.id_producto,
         nombre: p.nombre,
-        cantidad_pedida: Number(p.cantidad), // ← AQUÍ ESTÁ EL FIX
+        cantidad_pedida: p.cantidad, // ← AQUÍ ESTÁ EL FIX
         cantidad_entregada: Number(p.cantidad)
       }))
     )
