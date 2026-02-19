688a50f7f7d2bc42b1c682798f6106828d3ec816
GrupoTEKC <adhesivospegatek@gmail.com>
2026-02-19 16:05:56 -0600

    Update index.js

--- erp-backend/index.js
+++ erp-backend/index.js
@@ -36,30 +36,7 @@ app.get('/', (_, res) => {
 // OBTENER CLIENTE POR ID
 // =========================
 app.get('/clientes/:id', async (req, res) => {
-  try {
-    const { id } = req.params
-
-    const [rows] = await db.query(
-      `
-      SELECT *
-      FROM clientes
-      WHERE id_cliente = ?
-      `,
-      [id]
-    )
-
-    if (rows.length === 0) {
-      return res.status(404).json({ error: 'Cliente no encontrado' })
-    }
-
-    res.json(rows[0])
-
-  } catch (err) {
-    console.error('🔥 ERROR CLIENTE:', err)
-    res.status(500).json({ error: err.message })
-  }
-})
-
+  console.log("👉 Ruta cliente por ID ejecutada");
 
 app.post('/clientes', async (req, res) => {
   try {
