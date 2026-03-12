02eb792adeb21fde31f58b0b14adfa7e7540787a
GrupoTEKC <adhesivospegatek@gmail.com>
2026-03-12 14:19:19 -0600

    Update ConsultarPedidos.jsx

--- erp-frontend/src/pages/ConsultarPedidos.jsx
+++ erp-frontend/src/pages/ConsultarPedidos.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const API = import.meta.env.VITE_API_URL
+const API = 'https://erp-proyecto-production.up.railway.app'
 
 /* ===== ESTILOS ===== */
 
