d6daa325f86e0ee81420257a02862769b7be3566
GrupoTEKC <adhesivospegatek@gmail.com>
2026-03-18 09:52:25 -0600

    Update ProductosPedido.jsx

--- erp-frontend/src/components/ProductosPedido.jsx
+++ erp-frontend/src/components/ProductosPedido.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { API_URL } from '../config'
+const API_URL = 'https://erp-proyecto-production.up.railway.app'
 
 function ProductosPedido({ onTotalChange, onProductosChange }) {
 
