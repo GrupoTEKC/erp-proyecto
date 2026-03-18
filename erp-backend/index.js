f39e25bf0907a72ca90eed4ab970fd49a7661bd4
GrupoTEKC <adhesivospegatek@gmail.com>
2026-03-18 16:24:35 -0600

    Update index.js

--- erp-backend/index.js
+++ erp-backend/index.js
@@ -1,4 +1,5 @@
-console.log("🔥 VERSION 18 MARZO FORZADA 🔥")
+console.log("🔥 VERSION:", "18 MARZO");
+console.log("🌐 DB:", process.env.DB_NAME);
 require('dotenv').config()
 const express = require('express')
 const mysql = require('mysql2/promise')
