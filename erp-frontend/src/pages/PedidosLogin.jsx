c3a94d3d5656ace5d96e3c80b005ba3d432db548
GrupoTEKC <adhesivospegatek@gmail.com>
2026-04-08 15:53:01 -0600

    Create PedidosLogin.jsx

---
+++ erp-frontend/src/pages/PedidosLogin.jsx
@@ -0,0 +1,107 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import logo from '../assets/TRANSPARENTE.png'
+
+const styles = {
+  container: {
+    minHeight: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+    fontFamily: 'Arial'
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: '30px',
+    borderRadius: '10px',
+    width: '320px',
+    textAlign: 'center',
+    boxShadow: '0 4px 10px rgba(0,0,0,0.1)'
+  },
+  logo: {
+    width: '100px',
+    height: '100px',
+    borderRadius: '50%',
+    objectFit: 'cover',
+    marginBottom: '15px'
+  },
+  title: {
+    marginBottom: '20px',
+    color: '#071849'
+  },
+  input: {
+    width: '100%',
+    padding: '10px',
+    marginBottom: '10px',
+    borderRadius: '6px',
+    border: '1px solid #ccc'
+  },
+  button: {
+    width: '100%',
+    padding: '10px',
+    backgroundColor: '#8B1E1E',
+    color: '#fff',
+    border: 'none',
+    borderRadius: '6px',
+    cursor: 'pointer'
+  },
+  error: {
+    color: 'red',
+    marginBottom: '10px'
+  }
+}
+
+function PedidosLogin() {
+  const navigate = useNavigate()
+
+  const [usuario, setUsuario] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
+
+  const handleLogin = () => {
+    if (
+      usuario === 'vendedor#01' &&
+      password === 'Npddven26!_'
+    ) {
+      navigate('/nuevo-pedido') // 👈 ruta a tu módulo de pedidos
+    } else {
+      setError('Usuario o contraseña incorrectos')
+    }
+  }
+
+  return (
+    <div style={styles.container}>
+      <div style={styles.card}>
+        <img src={logo} alt="Grupo TEKC" style={styles.logo} />
+
+        <h2 style={styles.title}>Nuevo Pedido</h2>
+        <p style={{ marginBottom: '15px', color: '#555', fontSize: '13px' }}>
+        Para agregar un nuevo pedido deberá iniciar sesión
+        </p>
+        {error && <div style={styles.error}>{error}</div>}
+
+        <input
+          style={styles.input}
+          placeholder="Usuario"
+          value={usuario}
+          onChange={(e) => setUsuario(e.target.value)}
+        />
+
+        <input
+          type="password"
+          style={styles.input}
+          placeholder="Contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button style={styles.button} onClick={handleLogin}>
+          Iniciar sesión
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default PedidosLogin
