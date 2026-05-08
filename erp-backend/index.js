602b563fd8de7e79def5aa4381778cb92a533428
GrupoTEKC <adhesivospegatek@gmail.com>
2026-05-08 14:08:27 -0600

    Update index.js

--- erp-backend/index.js
+++ erp-backend/index.js
@@ -822,23 +822,30 @@ app.get('/pedidos/:id/detalle', async (req, res) => {
   pd.precio_unitario AS precio,
   pd.cantidad AS cantidad_pedida,
   COALESCE(prd.cantidad_planeada, pd.cantidad) AS cantidad_planeada,
-  pp.id_chofer,
-  pp.id_unidad,
+ COALESCE(e.id_chofer, pp.id_chofer) AS id_chofer,
+COALESCE(e.id_unidad, pp.id_unidad) AS id_unidad,
   CONCAT(ch.nombre,' ',ch.apellido1,' ',ch.apellido2) AS chofer,
   u.nombre AS unidad
       FROM pedido_detalle pd
       INNER JOIN productos p
         ON p.id_producto = pd.id_producto
-      LEFT JOIN programaciones_pedido pp
-        ON pp.id_pedido = pd.id_pedido
-        AND pp.activo = 1
-      LEFT JOIN programacion_detalle prd
-        ON prd.id_programacion = pp.id_programacion
-        AND prd.id_producto = pd.id_producto
-      LEFT JOIN choferes ch
-        ON ch.id_chofer = pp.id_chofer
-      LEFT JOIN unidades u
-        ON u.id_unidad = pp.id_unidad
+    LEFT JOIN programaciones_pedido pp
+  ON pp.id_pedido = pd.id_pedido
+  AND pp.activo = 1
+
+LEFT JOIN entregas e 
+  ON e.id_pedido = pd.id_pedido 
+  AND e.estado IN ('en_ruta','entregado')
+
+LEFT JOIN programacion_detalle prd
+  ON prd.id_programacion = pp.id_programacion
+  AND prd.id_producto = pd.id_producto
+
+LEFT JOIN choferes ch
+  ON ch.id_chofer = COALESCE(e.id_chofer, pp.id_chofer)
+
+LEFT JOIN unidades u
+  ON u.id_unidad = COALESCE(e.id_unidad, pp.id_unidad)
       WHERE pd.id_pedido = ?
     `, [id])
 
