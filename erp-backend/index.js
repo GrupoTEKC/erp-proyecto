8ce77b906d5c9c9c295b8d3b995878d56cd2c86d
GrupoTEKC <adhesivospegatek@gmail.com>
2026-06-15 09:33:12 -0600

    Update index.js

--- erp-backend/index.js
+++ erp-backend/index.js
@@ -1265,22 +1265,24 @@ if (productoPedido.length > 0) {
         console.log('✏️ UPDATE NORMAL')
 
         await conn.query(`
-          UPDATE entrega_detalle
-          SET 
-            cantidad_entregada = ?,
-            tipo = ?,
-            motivo = ?,
-            id_cliente_destino = ?
-          WHERE id_entrega = ? AND id_producto = ?
+        UPDATE entrega_detalle
+        SET
+        cantidad_final = ?,
+        tipo = ?,
+        motivo = ?,
+        id_cliente_destino = ?
+        WHERE id_entrega = ? AND id_producto = ?
         `, [
-          item.cantidad_entregada || 0,
-          item.tipo || 'ninguno',
-         (item.tipo === 'roto' || item.tipo === 'con_incremento')
-         ? (item.motivo || null)
-         : null,
-          item.tipo === 'prestamo' ? (item.id_cliente_destino || null) : null,
-          id_entrega,
-          item.id_producto
+        item.cantidad_entregada || 0,
+        item.tipo || 'ninguno',
+        (item.tipo === 'roto' || item.tipo === 'con_incremento')
+        ? (item.motivo || null)
+        : null,
+        item.tipo === 'prestamo'
+        ? (item.id_cliente_destino || null)
+        : null,
+        id_entrega,
+        item.id_producto
         ])
         
 if (item.tipo === 'con_incremento') {
