5a95e6c646290754ef39ee75d373e4abae24f2e3
GrupoTEKC <adhesivospegatek@gmail.com>
2026-07-07 16:39:33 -0600

    Update Pagos.jsx

--- erp-frontend/src/pages/Pagos.jsx
+++ erp-frontend/src/pages/Pagos.jsx
@@ -552,7 +552,7 @@ return (
     style={{
       margin: 0,
       color: "#8B1E1E", // rojo institucional
-      fontSize: "26px", // aproximadamente la mitad del tamaño actual
+      fontSize: "28px", // aproximadamente la mitad del tamaño actual
       fontWeight: "bold",
       letterSpacing: "1px"
     }}
